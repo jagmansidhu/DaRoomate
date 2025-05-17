ef1d230c86d5ce354c45cbd9ffd44704c15260ee
Jagmans <jagmansidhu1@gmail.com>
2025-05-17 16:13:44 -0700

    Implemented email password reset, and verfication link

--- backend/src/main/java/com/roomate/app/util/EmailUtil.java
+++ backend/src/main/java/com/roomate/app/util/EmailUtil.java
@@ -6,9 +6,17 @@ public class EmailUtil {
                 "Please click on the link below to verify your account:\n" +
                 getVerificationLink(host, token) + "\nSupport";
     }
+    public static String getPasswordMessage(String name, String host, String token) {
+        return "Hello " + name + ",\n" +
+                "Please click on the link below to reset your password:\n" +
+                getPasswordLink(host, token) + "\nSupport";
+    }
 
     public static String getVerificationLink(String host, String token) {
         return host + "/verify/account?token=" + token;
+    }
+    public static String getPasswordLink(String host, String token) {
+        return host + "/verify/password?token=" + token;
     }
 
 }
