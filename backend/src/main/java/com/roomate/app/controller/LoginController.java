06575b721bfdc0a06578aa1ed7e8f284bddc3022
Jagmans <jagmansidhu1@icloud.com>
2025-05-28 21:17:39 -0700

    Adding frontend folder

--- backend/src/main/java/com/roomate/app/controller/LoginController.java
+++ backend/src/main/java/com/roomate/app/controller/LoginController.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
