25b7d6dc3ebb9f7761946b95273ae1981d217ba5
Jagmans <jagmansidhu1@icloud.com>
2025-06-07 17:19:31 -0700

    Added tests for UserRepository

--- backend/src/test/java/com/example/demo/StartOneApplicationTests.java
+++ backend/src/test/java/com/example/demo/StartOneApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
+import com.roomate.app.StartOneApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class StartOneApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(StartOneApplication.class, args);
 	}
 
 }
---
+++ backend/src/test/java/com/example/demo/repositoryTest/UserRepositoryTest.java
@@ -0,0 +1,4 @@
+package com.example.demo.repositoryTest;
+
+public class UserRepositoryTest {
+}
